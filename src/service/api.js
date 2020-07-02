b323ed9efd0cf6ea5a58ce39c18950986129dba8
luiz medeiros <luiz.medeiros@fabricainfo.com>
2020-07-02 15:27:32 -0300

    criando um serviço para o axios

---
+++ src/service/api.js
@@ -0,0 +1,53 @@
+import http from './http';
+
+export default class API {
+  constructor(api) {
+    this.api = api;
+    this.http = http;
+  }
+
+  list = async () => {
+    try {
+      const response = await http.get(this.api);
+      return response.data;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  show = async ($username) => {
+    try {
+      const response = await http.get(`${this.api}/${$username}/repos`);
+      return response.data;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  create = async ($data) => {
+    try {
+      const response = await http.post(this.api, $data);
+      return response.data;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  update = async ($data) => {
+    try {
+      const response = await http.put(`${this.api}/${$data.id}`, $data);
+      return response.data;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  remove = async ($id) => {
+    try {
+      const response = await http.delete(`${this.api}/${$id}`);
+      return response.data;
+    } catch (error) {
+      return error;
+    }
+  }
+}
---
+++ src/service/repositorio/RepositorioService.js
@@ -0,0 +1,7 @@
+import Api from '../api';
+
+export default class RepostorioService extends Api {
+  constructor() {
+    super('/users');
+  }
+}
