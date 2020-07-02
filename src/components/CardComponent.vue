b00bfb6e80edee8fbe10a951b5ca57e25996fec7
luiz medeiros <luiz.medeiros@fabricainfo.com>
2020-07-02 17:23:30 -0300

    criando um componente de card

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card>
+    <v-card-title>{{title}}</v-card-title>
+    <v-card-subtitle>{{subtitle}}</v-card-subtitle>
+  </v-card>
+</template>
+<script>
+export default {
+  name: 'CardComponent',
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    subtitle: {
+      type: String,
+      default: '',
+    },
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
