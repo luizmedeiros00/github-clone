df9e0b327b34d62722be6e6efa369f5d6a510943
luiz medeiros <luiz.medeiros@fabricainfo.com>
2020-07-02 17:11:40 -0300

    instalando o vuetify

---
+++ src/components/ListaRepositorios.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(item, key) in repos" :key="key">
+        {{item.name}} <br>
+        <strong>{{item.description}}</strong>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ListaRepositorios',
+  props: {
+    repos: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,32 @@
 <template>
   <div class="home">
+    <lista-repositorios :repos="repos" />
   </div>
 </template>
 
 <script>
+import RepositorioService from '@/service/repositorio/RepositorioService';
+import ListaRepositorios from '@/components/ListaRepositorios.vue';
 
 export default {
   name: 'Home',
   components: {
+    ListaRepositorios,
+  },
+  data() {
+    return {
+      RepositorioService: new RepositorioService(),
+      repos: [],
+    };
+  },
+  methods: {
+    async onLoadRepositorio() {
+      const data = await this.RepositorioService.show('camunda');
+      this.repos = [...data];
+    },
+  },
+  created() {
+    this.onLoadRepositorio();
   },
 };
 </script>
